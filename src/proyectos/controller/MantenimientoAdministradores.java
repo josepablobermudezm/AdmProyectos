8684ab233aec73356e5ba50b70eb1639c562084d
Brandon mata <brand9613@hotmail.com>
2019-09-14 00:10:37 -0600

    Revert "Revert "txtformats""
    This reverts commit d462cad10d0633fdcac7191660387008278c92cd.

--- src/proyectos/controller/MantenimientoAdministradores.java
+++ src/proyectos/controller/MantenimientoAdministradores.java
@@ -11,6 +11,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import proyectos.util.Formato;
 import proyectos.util.Mensaje;
 import proyectos.util.Respuesta;
 
@@ -128,5 +129,16 @@ public class MantenimientoAdministradores extends Controller {
 
     @FXML
     private void agregar(ActionEvent event) {
+    }
+    //metodos extra
+    // Da formato a los distintos elementos graficos
+    public void iniciarObjetos(){
+        txtNombre.setTextFormatter(Formato.getInstance().letrasFormat(20));
+        txtContraseña.setTextFormatter(Formato.getInstance().maxLengthFormat(8));
+        txtCorreo.setTextFormatter(Formato.getInstance().maxLengthFormat(20));
+        txtPapellido.setTextFormatter(Formato.getInstance().letrasFormat(20));
+        txtSapellido.setTextFormatter(Formato.getInstance().letrasFormat(20));
+        txtUsuario.setTextFormatter(Formato.getInstance().maxLengthFormat(20));
+        txtCedula.setTextFormatter(Formato.getInstance().cedulaFormat(10)); //revisar
     }
 }
