1a80a03c184a65297ba1dbadf9189e83fbc3030f
Brandon mata <brand9613@hotmail.com>
2019-09-20 20:05:02 -0600

    respaldo

---
+++ src/proyectos/controller/PantallaInicioController.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectos.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author Bran
+ */
+public class PantallaInicioController implements Initializable {
+
+    @FXML
+    private Label label_nombre001;
+    @FXML
+    private Label label_Usuario001;
+    @FXML
+    private Label label_pass001;
+    @FXML
+    private Label label_PApellido001;
+    @FXML
+    private Label label_SApellido;
+    @FXML
+    private Label label_Correo001;
+    @FXML
+    private JFXButton btn_Regstrarse001;
+    @FXML
+    private JFXButton btn_YTCuenta001;
+    @FXML
+    private JFXTextField tField_nombre001;
+    @FXML
+    private JFXTextField tField_PApellido001;
+    @FXML
+    private JFXTextField tField_SApellido001;
+    @FXML
+    private JFXTextField tField_Correo001;
+    @FXML
+    private JFXTextField tField_Usuario001;
+    @FXML
+    private JFXTextField tField_pass001;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
