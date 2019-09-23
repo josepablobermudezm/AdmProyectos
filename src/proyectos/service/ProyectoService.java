12eddca1cd5d0e5d052738977745463f88309e3c
josepablobermudezm <53883209+josepablobermudezm@users.noreply.github.com>
2019-09-22 18:03:29 -0600

    Update ProyectoService.java

--- src/proyectos/service/ProyectoService.java
+++ src/proyectos/service/ProyectoService.java
@@ -5,9 +5,13 @@
  */
 package proyectos.service;
 
+import java.net.ConnectException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import proyectos.model.AdministradorDto;
 import proyectos.model.ProyectoDto;
 import proyectos.util.DtoCasting;
-import proyectos.util.Respuesta;
+import webservice.Respuesta;
 import webservice.WS;
 import webservice.WS_Service;
 
@@ -16,14 +20,31 @@ import webservice.WS_Service;
  * @author Jose Pablo Bermudez
  */
 public class ProyectoService {
-     WS_Service service = new webservice.WS_Service();
+    WS_Service service = new webservice.WS_Service();
     WS webService = service.getWSPort();
-    DtoCasting dtoCasting = new DtoCasting();
+    DtoCasting dtoCast = new DtoCasting();
+ 
+    public proyectos.util.Respuesta guardarAdministrador(ProyectoDto pro) {
+        try {
+            webservice.ProyectoDto proyecto = dtoCast.castPro(pro);
+            webservice.Respuesta resp = webService.guardarProyecto(proyecto);
+            if (!resp.isEstado()) {
+                return new proyectos.util.Respuesta(false, resp.getMensaje(), resp.getMensajeInterno(), "", "");
+            }
+            return new proyectos.util.Respuesta(true, resp.getMensaje(), resp.getMensajeInterno(), "", "");
+        } catch (Exception ex) {
+            Logger.getLogger(AdministradorService.class.getName()).log(Level.SEVERE, "Error guardando el Proyecto.", ex);
+            return new proyectos.util.Respuesta(false, "Error guardando el Usuario.", "guardarProyecto " + ex.getMessage());
+        }
+    }
     
-    public Respuesta GuardarProyecto(ProyectoDto proyectoDto){
-         webservice.ProyectoDto  proyectoDto1 = dtoCasting.castProyecto(proyectoDto);
-         webservice.Respuesta  res = webService.guardarProyecto(proyectoDto1);
-        return null;
-        
+    public proyectos.util.Respuesta eliminarProyecto(Long Id) {
+        try {
+            String mensaje = webService.eliminarProyecto(Id);
+            return new proyectos.util.Respuesta(true, mensaje, "");
+        } catch (Exception ex) {
+            Logger.getLogger(AdministradorService.class.getName()).log(Level.SEVERE, "Error guardando el Usuario.", ex);
+            return new proyectos.util.Respuesta(false, "Error guardando el Usuario.", "guardarUsuario " + ex.getMessage());
+        }
     }
 }
